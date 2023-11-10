f235090b0a3b814418648ca95bafb5dbfde613a1
Viktor Vida <wityamin@gmail.com>
2023-11-10 18:31:59 +0100

    Floor prefabs

---
+++ Assets/Scripts/RandomizeFloorDetail.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomizeFloorDetail : MonoBehaviour
+{
+    public GameObject detail;
+    public float radius;
+    void Awake()
+    {
+        float defaultHeight = detail.transform.position.y;
+        detail.transform.position = new Vector3(Random.Range(-radius, radius), defaultHeight, Random.Range(-radius, radius));
+        detail.transform.rotation = Quaternion.Euler(0, Random.Range(0, 360), 0);
+    }
+}
